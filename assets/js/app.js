23e2fa91eab80077dab44803aa0f990e8dd4580f
Michael Belete <50682242+michaelbelete@users.noreply.github.com>
2021-01-25 07:15:01 +0300

    added validation for add task

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,36 +1,121 @@
 // Define UI Variables 
-
 const taskInput = document.querySelector('#task'); //the task input text field
-
 const form = document.querySelector('#task-form'); //The form at the top
-
 const filter = document.querySelector('#filter'); //the task filter text field
-
-const taskList = document.querySelector('.collection'); //The ul
-
+const taskList = document.querySelector('.collection'); //The UL
 const clearBtn = document.querySelector('.clear-tasks'); //the all task clear button
+const reloadIcon = document.querySelector('.fa'); //the reload button at the top navigation 
+const message = document.querySelector('#message')
+// Add Event Listener  [Form , clearBtn and filter search input ]
 
 // form submit 
-form.addEventListener('submit', addNewTask); //this are first order function
-
+form.addEventListener('submit', addNewTask);
 // Clear All Tasks
 clearBtn.addEventListener('click', clearAllTasks);
-
 //   Filter Task 
 filter.addEventListener('keyup', filterTasks);
+// Remove task event [event delegation]
+taskList.addEventListener('click', removeTask);
+// Event Listener for reload 
+reloadIcon.addEventListener('click', reloadPage);
 
 
-// event handlers
 
+function createMessage(msg, color){
+    message.innerText = msg;
+    message.className = `card-panel white-text ${color}`
+    message.style.display = "block"
+
+    const del = document.createElement('a')
+    del.classList = 'delete-item secondary-content'
+    del.innerHTML = '<i class="fa fa-remove"></i>'
+
+    message.appendChild(del)
+
+}
+// Add New  Task Function definition 
 function addNewTask(e) {
-    e.preventDefault();
-    alert("add new task");
+
+    e.preventDefault(); //disable form submission
+
+    // Check empty entry
+    if (taskInput.value === '') {
+        taskInput.style.borderColor = "red";
+        createMessage("Please fill out the required form", "red")
+        return;
+    }
+
+    // Create an li element when the user adds a task 
+    const li = document.createElement('li');
+    // Adding a class
+    li.className = 'collection-item';
+    // Create text node and append it 
+    li.appendChild(document.createTextNode(taskInput.value));
+    // Create new element for the link 
+    const link = document.createElement('a');
+    // Add class and the x marker for a 
+    link.className = 'delete-item secondary-content';
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    // Append link to li
+    li.appendChild(link);
+    // Append to UL 
+    taskList.appendChild(li);
+
+    taskInput.value = "" //clearing the input
+
 }
 
-function clearAllTasks(e) {
-    alert("clear all tasks");
+
+
+
+
+// Clear Task Function definition 
+function clearAllTasks() {
+
+    //This is the first way 
+    // taskList.innerHTML = '';
+
+    //  Second Wy 
+    while (taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    }
+
 }
 
-function filterTasks(e){
-    console.log("searching...")
+
+
+// Filter tasks function definition 
+function filterTasks(e) {
+
+    /*  
+    Instruction for Handling the Search/filter 
+    
+    1. Receive the user input from the text input 
+    2. Assign it to a variable so the us can reuse it 
+    3. Use the querySelectorAll() in order to get the collection of li which have  .collection-item class 
+    4. Iterate over the collection item Node List using forEach
+    5. On each element check if the textContent of the li contains the text from User Input  [can use indexOf]
+    6 . If it contains , change the display content of the element as block , else none
+    
+    
+    */
+
+}
+
+// Remove Task function definition 
+function removeTask(e) {
+    if (e.target.parentElement.classList.contains('delete-item')) {
+        if (confirm('Are You Sure about that ?')) {
+            e.target.parentElement.parentElement.remove();
+
+        }
+
+    }
+}
+
+
+// Reload Page Function 
+function reloadPage() {
+    //using the reload fun on location object 
+    location.reload();
 }
