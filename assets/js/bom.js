75db59c4e9f3be4bc4c864d0cd2f2db73f017e66
Michael Belete <50682242+michaelbelete@users.noreply.github.com>
2021-01-29 23:44:41 +0300

    finished BOM exerise

--- assets/js/bom.js
+++ assets/js/bom.js
@@ -19,12 +19,44 @@ Good Luck !!!
 
 // Define UI Variables  here 
 
+const href = document.querySelector('#href')
+const protocol = document.querySelector('#protocol')
+const host = document.querySelector('#host')
+const port = document.querySelector('#port')
+const hostname = document.querySelector('#hostname')
 
+const appName = document.querySelector("#appName")
+const appVersion = document.querySelector("#appVersion")
+const platform = document.querySelector("#platform")
+const language = document.querySelector("#language")
+const cookieEnabled = document.querySelector("#cookieEnabled")
 
+const height = document.querySelector('#height')
+const width = document.querySelector('#width')
+const pixelDepth = document.querySelector('#pixelDepth')
 
-
+const length = document.querySelector('#length')
+const state = document.querySelector('#state')
 
 
 
 
 // Display the BOM Information on the innerHTML of the elements
+href.innerHTML = location.href
+protocol.innerHTML = location.protocol
+host.innerHTML = location.host
+port.innerHTML = location.port
+hostname.innerHTML = location.hostname
+
+appName.innerHTML = navigator.appName
+appVersion.innerHTML = navigator.appVersion
+platform.innerHTML = navigator.platform
+language.innerHTML = navigator.language
+cookieEnabled.innerHTML = navigator.cookieEnabled 
+
+height.innerHTML = screen.height
+width.innerHTML = screen.width
+pixelDepth.innerHTML = screen.pixelDepth
+
+length.innerHTML = history.length
+state.innerHTML = history.state
